67980a0c94821396ee40cdbfdb5d168642948e8c
OneCloudInSky <gleb_zvor@mail.ru>
2022-03-24 08:51:21 +0300

    realloc added, scanf fixed, interactions with the console are out of the functions

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -5,7 +5,7 @@
 #define MALLOC_ERROR "ERROR:MALLOC ERROR\n"
 #define REALLOC_ERROR "ERROR:REALLOC ERROR\n"
 #define PUSH_ERROR "ERROR: STACK IS OUT OF RANGE\n"
-#define PEAK_ERROR "ERROR: STACK IS OUT OF RANGE\n"
+#define POP_ERROR "ERROR: STACK IS OUT OF RANGE\n"
 #define OPTION_CHOOSE_ERROR "ERROR:CHOOSE ANOTHER OPTION\n"
 #define STACK_MAX_SIZE 20
 
@@ -16,80 +16,90 @@ typedef struct arr { //создание структуры
 	int size;
 } stack_t;
 
+void realloc_data(stack_t *st) {
+	int *tmp_ptr = (int*)realloc(st->ptr, sizeof(int) * st->size);
+	if (tmp_ptr != NULL) {
+		st->ptr = tmp_ptr;
+	}
+	else if (st->size == 0) {
+		st->ptr = NULL;
+	}
+	else {
+		printf(REALLOC_ERROR);
+		free(st->ptr);
+		exit(0);
+	}
+	
+}
+
 void create(stack_t *st) { //создание стэка
 	st->size = 0;
-	st->ptr = (int*)malloc(sizeof(int) * st->size);
-	if (st->ptr == NULL) {
-		exit(MALLOC_ERROR);
-	}
+	st->ptr = NULL;
 }
 
-void clear(stack_t *st) { //очищение стека
-	while (st->size != 0) {
-		printf("\ndeleted element is: ");
-		printf("%i", st->ptr[st->size - 1]);
-		st->ptr[st->size - 1] = NULL;
-		st->size--;
-	}
-	printf("no elements to delete\n#######\n#CLEAN#\n#######\n");
-}
-
-void check_scanf(int *a) { //проверка на корректность ввода
-	if (a == NULL) {
-		exit(SCANF_ERROR);
-	}
-}
-
-int get_new_el() { //создание нового элемента для функции push
-	int element;	
-	printf("enter new element: ");
-	scanf("%i", &element);
-	check_scanf(&element);
-	return element;	
-}
-
-void push(stack_t *st) { //добавление элемента в стек
+void push(stack_t *st, int a) { //добавление элемента в стек
 	if (st->size == STACK_MAX_SIZE) {
 		printf(PUSH_ERROR);
 	}
 	else {
-		st->ptr[st->size] = get_new_el();
 		st->size++;
+		realloc_data(st);
+		st->ptr[st->size - 1] = a;
 	}
 }
 
-void peak(stack_t *st) { //извлечение верхушки стека
+int pop (stack_t *st) { //извлечение верхушки стека
 	if (st->size != 0) {
-		printf("\npeak element is: ");
-		printf("%i", st->ptr[st->size - 1]);
-		st->ptr[st->size - 1] = NULL;
+		int tmp = st->ptr[st->size - 1];
+		st->ptr[st->size - 1] = -1;
 		st->size--;
+		realloc_data(st);
+		return tmp;
 	}
-	else
-		printf(PEAK_ERROR);
 }
 
 void task() { //список заданий
-	printf("\nclear the stack(1)\nadd an element(2)\nget the peak of stack(3)\nswap 2 top elements(4)\ndelete the bottom element(5)\nswap top and bottom(6)\nend(0)\n\n");
+	printf("\nclear the stack(1)\nadd an element(2)\npop the element(3)\nswap 2 top elements(4)\ndelete the bottom element(5)\nswap top and bottom(6)\nend(0)\n\n");
 }
 
 void menu(stack_t *st) { //меню выбора заданий
 	int choose;
 	task();
+	int new_element;
+	int tmp_pop;
+	int tmp_clean;
 	while (choose != 0){
 		int choose;
 		printf("\nchoose the option(press 10 to show the task): ");
-		scanf("%i", &choose);
-		check_scanf(&choose);
+		int check = scanf("%i", &choose);
+		while (check != 1) {
+			printf(SCANF_ERROR);
+			printf("\nchoose the option(press 10 to show the task): ");
+			int check = scanf("%i", &choose);
+		}
 		switch(choose) {
 			case 1:
-				clear(st);
+				while (st->size != 0) {
+					printf("\ndeleted element is: ");
+					printf("%i\n", st->ptr[st->size - 1]);
+					pop(st);
+				}
+				if (st->size == 0)
+					printf("\n     #######\n     #CLEAN#\n     #######\n");
 				break;
 			case 2:
-				push(st);
+				printf("enter new element: ");
+				scanf("%i", &new_element);
+				push(st, new_element);
 				break;
 			case 3:
-				peak(st);
+				if (st->size != 0) {
+					printf("poped element is: ");
+					printf("%i", st->ptr[st->size - 1]);
+					pop(st);
+				}
+				else
+					printf(POP_ERROR);
 				break;
 			case 4:
 				//
@@ -120,4 +130,3 @@ int main() {
 	create(&st1);
 	menu(&st1);
 }
-
